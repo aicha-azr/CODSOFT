b08066f7b5c4190f82c26053673802db6d42ff46
aicha-azr <aichaazeroual03@gmail.com>
2024-08-26 16:59:44 +0100

    create Models

---
+++ Blog_Platform/server/Models/PostSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// Define the Post schema
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Create the Post model
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
--- Blog_Platform/server/index.js
+++ Blog_Platform/server/index.js
@@ -1,1 +1,11 @@
- 
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => { 
+    res.json('hello world');
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
