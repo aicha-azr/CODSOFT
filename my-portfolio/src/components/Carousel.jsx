453f493969aca8c519ea13a76efb45dff8043435
aicha-azr <aichaazeroual03@gmail.com>
2024-09-06 11:20:27 +0100

    update projects section

--- my-portfolio/src/components/Carousel.jsx
+++ my-portfolio/src/components/Carousel.jsx
@@ -3,11 +3,14 @@ import projects from "../db/projects";
 import { useEffect, useState } from "react";
 import { Link } from "lucide-react";
 import Aos from "aos";
- 
+import 'aos/dist/aos.css';
 export function CarouselCustomNavigation() {
   const [filteredProjects, setFilteredProjects] = useState(projects);
   const [displayedIndex, setDisplayedIndex] = useState(null);
-  const filtrage = (value) => {
+ const [clickedEvent, setClickEvent] = useState('All');
+const filtrage = (value) => {
+  Aos.refresh();
+    setClickEvent(value);
     if (value === "All") {
       setFilteredProjects(projects);
     } else {
@@ -18,32 +21,47 @@ export function CarouselCustomNavigation() {
     Aos.init({
         duration: 1000,
         easing: 'ease-in-out',
-        once: true 
-      },[]);
-  });
+        once: false 
+      });
+    }, [ ]); 
+  ;
   return (
     <div className=' flex  flex-col gap-3'>
       <div className="flex justify-around md:gap-6 md:w-1/2 text-black md:pl-4 font-medium">
-        <p className="  p-1 px-2 focus:outline-none hover:text-[#F564AB]  hover:cursor-pointer" onClick={() => filtrage("All")}>All</p>
-        <p className="  focus:outline-none p-1 hover:text-[#F564AB] hover:cursor-pointer" onClick={() => filtrage("Full Stack")}>Full Stack</p>
-        <p className="  focus:outline-none p-1 hover:text-[#F564AB] hover:cursor-pointer" onClick={() => filtrage("Frontend")}>Frontend</p>
-        <p className="  focus:outline-none p-1 hover:text-[#F564AB]  hover:cursor-pointer" onClick={() => filtrage("Backend")}>Backend</p>
+        <p className={`focus:outline-none p-1 hover:text-b1 hover:bg-gray-200 hover:cursor-pointer p-2 rounded-full ${clickedEvent === 'All'?'bg-gray-200 text-b1':'bg-transparent'}`} onClick={() => filtrage("All")}>All</p>
+        <p className={`focus:outline-none p-1 hover:text-b1 hover:bg-gray-200 hover:cursor-pointer p-2 rounded-full ${clickedEvent === 'Full Stack'?'bg-gray-200 text-b1':'bg-transparent'}`} onClick={() => filtrage("Full Stack")
+        }>Full Stack</p>
+        <p className={`focus:outline-none p-1 hover:text-b1[#F564AB] hover:bg-gray-200 hover:bg-gray-200 hover:cursor-pointer p-2 rounded-full ${clickedEvent === 'Frontend'?'bg-gray-200 text-b1':'bg-transparent'}`} onClick={() => filtrage("Frontend")}>Frontend</p>
+        <p className={`focus:outline-none p-1 hover:text-b1 hover:bg-gray-200 hover:cursor-pointer p-2 rounded-full ${clickedEvent === 'Backend'?'bg-gray-200 text-b1':'bg-transparent'}`} onClick={() => filtrage("Backend")}>Backend</p>
         </div>
-    <div className="  flex flex-wrap flex-row gap-8 p-3 items-center w-fit justify-start ">
+    <div className="  flex flex-wrap flex-row md:grid md:grid-cols-2 lg:grid-cols-3 gap-8 p-3 items-start w-fit justify-start ">
     {filteredProjects.map((item, index) => (
                 <div
                     key={index}
-                    className='flex flex-col gap-4 items-center md:w-1/2 lg:w-[400px] h-[15.5rem] rounded-[11px] shadow-md shadow-xl h-fit relative'
+                    className='flex flex-col gap-4 items-center md:w-1/2 lg:w-[400px] h-[15.5rem] rounded-[11px] shadow-beige shadow-xl bg-[#f9feff] md:w-full  h-fit relative hover:scale-0.5 transform cursor-pointer py-5'
                    
                     onMouseEnter={() => setDisplayedIndex(index)}
                     onMouseLeave={() => setDisplayedIndex(null)}
-                >
+                    >
+                   <div className="flex p-3  bg-gray-200 rounded-lg m-2">
                     <img
                         src={item.image}
                         alt={item.title}
-                        className="shadow-md rounded-lg w-full h-full hover:brightness-50"
-                    />
-                    <div className="absolute left-1 bottom-2 backdrop-opacity-10">
+                        className="shadow-md rounded-lg w-full h-full "
+                    /> </div>
+                    <div className="flex flex-col text-start px-2">
+                    <h3 className={'text-BRoyal md:text-xl bg-brightness-50  font-semibold'}>{item.title}</h3>
+                   <div className="flex">
+                    <p className="text-[#023246] ">{item.description}</p>
+                    <a href={item.link} className="self-end">
+                        <Link  color="#023246" size={30} className="bg-pink rounded-full p-1 mx-2 "/></a>
+                   </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                  { /* <div className="absolute left-1 bottom-2 backdrop-opacity-10">
                         <h3 className={displayedIndex === index ?'hidden':'text-[#023246] md:text-xl bg-brightness-50 font-medium'}>{item.title}</h3>
                     </div>
                     <div className={displayedIndex === index ? 'block absolute h-fit min-h-full z-20 p-1 py-0 bg-beige text-blueGreen text-start rounded-xl flex flex-col justify-center bg-opacity-90' : 'hidden'} data-aos="fade-up">
@@ -52,7 +70,7 @@ export function CarouselCustomNavigation() {
                         <a href={item.link} className="absolute top-1 right-1">
                         <Link  color="#023246" size={30} className="bg-pink rounded-full p-1 mx-2 "/></a>
                         
-                    </div>
+                    </div>*/}
                 </div>
             ))}
     
--- my-portfolio/src/db/projects.jsx
+++ my-portfolio/src/db/projects.jsx
@@ -3,6 +3,48 @@ import Note from '../assets/notemanagement.jpg';
 import tech from '../assets/tech.jpg';
 const projects = [
     {
+        image:"/src/assets/tbp2.JPG",
+        link:"https://github.com/aicha-azr/CODSOFT/tree/main/Blog_Platform",
+        title:"Tech Blog Platform",
+        description:"A simple tech blog platform built with the MERN stack, featuring a rich text editor, image uploading, and JWT authentication. The platform allows users to create, update, and manage blog posts seamlessly.",
+        category: "Full Stack"
+        
+    },
+    {
+        image: Note,
+        link:"https://note-management.vercel.app/",
+        title:"Note management",
+        description:"This project aims to develop a simplified note-taking application for solo use. The strategy and project requirements are tailored to match the skills and resources of an individual developer. By focusing on the use of Next.js, Redux Toolkit, and TypeScript.",
+        category: "Full Stack"
+    },
+        {
+            image:"https://maghreb.simplonline.co/_next/image?url=https%3A%2F%2Fsimplonline-v3-prod.s3.eu-west-3.amazonaws.com%2Fmedia%2Fimage%2Fjpg%2Fbest-recipes-blogimage-v3-65968aa8d98a7659538858.jpg&w=1280&q=75",
+            link:"https://github.com/aicha-azr/Authentifier-_Recipe_API.git",
+            title:"Recipe APIs",
+            description:"Development of a robust API with Express.js and MongoDB for the \‘Online Recipe Book\’. It will allow for the exploration, addition, modification, and deletion of recipes. Express.js will handle HTTP requests and MongoDB will store the recipe data.",
+            category: "Backend"
+            
+        },
+        {
+            image:"https://maghreb.simplonline.co/_next/image?url=https%3A%2F%2Fsimplonline-v3-prod.s3.eu-west-3.amazonaws.com%2Fmedia%2Fimage%2Fjpg%2Fr-65a525af8532a451645825.jpg&w=1280&q=75",
+            link:"https://github.com/aicha-azr/AuthFlow-Pro.git",
+            title:"Implementing Authentication in the Online Cookbook API",
+            description:"This project focuses on enhancing API security by integrating an authentication system. Users will be able to create accounts, authenticate, and obtain access tokens to interact with the API. Sensitive routes, such as creating, updating, and deleting recipes, will be protected and accessible only to authenticated users. An error management system will be implemented to provide precise feedback, and the API documentation will be updated to include detailed instructions.",
+            category: "Backend"
+            
+        },{
+            image:"/src/assets/picture2.png",
+            link:"https://tasktracker-aichas-projects-14c2cccb.vercel.app/",
+            title:"Task Tracker App",
+            description:"This is a simple task tracker application built with React and styled with Tailwind CSS. The app, created using Vite for fast development and build processes, allows users to manage their to-do items by adding, updating, and deleting tasks. Users can also mark tasks as complete or incomplete.",
+            category: "Frontend"
+            
+        },,
+    ]
+    export default projects;
+    /**
+     
+    {
         image: CConnect,
         link:"https://cook-connect.vercel.app/",
         title:"Blog web site",
@@ -10,36 +52,13 @@ const projects = [
         category: "Full Stack"
     },
     {
-        image: Note,
-        link:"https://note-management.vercel.app/",
-        title:"Note management",
-        description:"Ce projet vise à développer une application de prise de notes performante et professionnelle. En utilisant Next.js, Redux Toolkit et TypeScript, l'application permet de créer, lire, éditer et supprimer des notes.\n Technologies Utilisées: \n _Next.js pour le développement front-end et le routage basé sur le système de fichiers, \n _Redux Toolkit pour la gestion de l'état de l'application.\n _TypeScript pour améliorer la qualité et la maintenabilité du code.",
-        category: "Full Stack"
-
-    },
-    {
-        image:tech,
-        link:"https://tech-innovate-two.vercel.app/",
-        title:"Tech innovate",
-        description:"Développement d'un site vitrine professionnel pour Tech Innovate avec Next.js et TypeScript, adapté à un développeur solo.\n Technologies:Next.js,TypeScript,TailwindCSS.",
-        category: "Frontend"
-
-    },
-    {
-        image:"https://maghreb.simplonline.co/_next/image?url=https%3A%2F%2Fsimplonline-v3-prod.s3.eu-west-3.amazonaws.com%2Fmedia%2Fimage%2Fjpg%2Fbest-recipes-blogimage-v3-65968aa8d98a7659538858.jpg&w=1280&q=75",
-        link:"https://github.com/aicha-azr/Authentifier-_Recipe_API.git",
-        title:"Recipe APIs",
-        description:"Développement d'une API robuste avec Express.js et MongoDB pour le 'Livre de Recettes en Ligne'. Elle permettra l'exploration, l'ajout, la modification et la suppression de recettes. Express.js gérera les requêtes HTTP et MongoDB stockera les données des recettes.",
-        category: "Backend"
-
-    },
-    {
-        image:"https://maghreb.simplonline.co/_next/image?url=https%3A%2F%2Fsimplonline-v3-prod.s3.eu-west-3.amazonaws.com%2Fmedia%2Fimage%2Fjpg%2Fr-65a525af8532a451645825.jpg&w=1280&q=75",
-        link:"https://github.com/aicha-azr/AuthFlow-Pro.git",
-        title:"Implementing Authentication in the Online Cookbook API",
-        description:"Ce projet se concentre sur l'amélioration de la sécurité de l'API en intégrant un système d'authentification. Les utilisateurs pourront créer des comptes, s'authentifier, et obtenir des jetons d'accès pour interagir avec l'API. Les routes sensibles, telles que la création, la mise à jour et la suppression de recettes, seront protégées et accessibles uniquement aux utilisateurs authentifiés. Un système de gestion des erreurs sera mis en place pour fournir des retours précis, et la documentation de l'API sera mise à jour pour inclure des instructions détaillées.",
-        category: "Backend"
-
-    },
-]
-export default projects;
+     image:tech,
+     link:"https://tech-innovate-two.vercel.app/",
+     title:"Tech innovate",
+     description:"Développement d'un site vitrine professionnel pour Tech Innovate avec Next.js et TypeScript, adapté à un développeur solo.\n Technologies:Next.js,TypeScript,TailwindCSS.",
+     category: "Frontend"
+ 
+ },
+ * 
+ * 
+ */
